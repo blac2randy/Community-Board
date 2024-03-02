daebcb9233e931084773be0642d1e73ffb77aa00
JosiahWel8 <147206260+blac2randy@users.noreply.github.com>
2024-03-02 02:52:54 -0500

    Add files via upload

---
+++ Games.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+
+const Games = () => {
+    return (
+        <div className="Game">
+            <table>
+                <tbody>
+                    <tr>
+                        <td className="Game pink">
+                            <div className="content">
+                                <img src="https://img2.storyblok.com/f/157036/1920x1080/3dacf66710/news-2.jpg" alt="" />
+                                <h6>Valhiem</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi1s56ChdWEAxVOrokEHRikAzEQFnoECD4QAQ&url=https%3A%2F%2Fwww.valheimgame.com%2F&usg=AOvVaw3IKYHG1amiMwAQS6Lbgncv&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game blue">
+                            <div className="content">
+                                <img src="https://media.npr.org/assets/img/2022/02/23/eldenring_21_4k-25120461292d0c3a0414.08944875_wide-319af7cfd3c138293a179491fcfc58e15405f536-s1100-c50.jpg" alt="" />
+                                <h6>Elden ring</h6>
+                                <a href= "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjTkoeegNWEAxVVC3kGHfaQATkQFnoECEYQAQ&url=https%3A%2F%2Fen.bandainamcoent.eu%2Felden-ring%2Felden-ring&usg=AOvVaw0b2zbSR2hO2m0nK7khKUJ_&opi=89978449"> 
+                                   <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game green">
+                            <div className="content">
+                                <img src="https://cdn.mos.cms.futurecdn.net/CH9RRPqpMT9zrvThikZRSV-1200-80.jpg" alt="" />
+                                <h6>Dark Souls 3</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjT9_vSgNWEAxULtokEHd80D84QFnoECHQQAQ&url=https%3A%2F%2Fen.bandainamcoent.eu%2Fdark-souls%2Fdark-souls-iii&usg=AOvVaw1K_7OqKnYMTEC_IJtHWE0G&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                        
+                            </div>
+                        </td>
+                        <td className="Game blue">
+                            <div className="content">
+                                <img src="https://assets.xboxservices.com/assets/ae/a1/aea14b35-9f68-435b-abc1-1a490d34931e.jpg?n=SoT_Sneaky-Slider-1084_Season-9_1600x675.jpg" alt="" />
+                                <h6>Sea of theives</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidqsC3hdWEAxVhg4kEHUTACPkQFnoECEsQAQ&url=https%3A%2F%2Fwww.seaofthieves.com%2F&usg=AOvVaw0mM8KvS3pT1nztV3IrZGmn&opi=89978449">
+                                    <button>Visit Wbesite</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game pink">
+                            <div className="content">
+                                <img src="https://store-images.s-microsoft.com/image/apps.2678.14492077886571533.a48e9a4a-99a7-44a9-9d97-9e8e72220a7c.6b98c506-61b7-4126-80b6-449f2ff0fb96?mode=scale&q=90&h=1080&w=1920&background=%23FFFFFF" alt="" />
+                                <h6>Minecraft</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiqjKPWhdWEAxVlg4kEHXcdDFoQFnoECCEQAQ&url=https%3A%2F%2Fwww.minecraft.net%2Fen-us&usg=AOvVaw0KjL2f3Uwei9Si5EF5Ky-1&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game green">
+                            <div className="content">
+                                <img src="https://i0.wp.com/xboxera.com/wp-content/uploads/2020/10/riskofrain.jpg?fit=1280%2C720&ssl=1" alt="" />
+                                <h6>Risk of rain 2</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjOpe77hdWEAxXMg4kEHaUnAMEQFnoECHcQAQ&url=https%3A%2F%2Fwww.riskofrain.com%2F&usg=AOvVaw0xESg4Kn1OyZ8S4H37-hji&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game pink">
+                            <div className="content">
+                                <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/gears-of-war-5/c/c8/Apps.39119.65494054591008504.3eb95796-405c-4def-901b-3b53a1c9e001.jpg" alt="" />
+                                <h6>Gears of war 5</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiWpcKkhtWEAxWbrYkEHYupDIcQFnoECGsQAQ&url=https%3A%2F%2Fwww.xbox.com%2Fen-US%2Fgames%2Fgears-5&usg=AOvVaw3t4-2OKYr4EkxqJKitZnQ0&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game blue">
+                            <div className="content">
+                                <img src="https://cdn.wccftech.com/wp-content/uploads/2019/06/bl3_mech_monster.jpg" alt="" />
+                                <h6>Borderlands 3</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi8trPBhtWEAxVEjYkEHYNJDJYQFnoECE8QAQ&url=https%3A%2F%2Fborderlands.2k.com%2F&usg=AOvVaw1bXe2hzZyY_vt3eGEqQRf4&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game pink">
+                            <div className="content">
+                                <img src="https://buenaspeaks.org/wp-content/uploads/2022/03/sdkjbvaijkvnadefsvve.jpeg" alt="" />
+                                <h6>Terria</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj-rN7ghtWEAxUmg4kEHVK5AU4QFnoECD0QAQ&url=https%3A%2F%2Fterraria.org%2F&usg=AOvVaw1p5mvJWoyhIMs68IFx1OpQ&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+                        <td className="Game blue">
+                            <div className="content">
+                                <img src="https://assetsio.reedpopcdn.com/Rocket-League-(header-suggestion).jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp" alt="" />
+                                <h6>Rocket league</h6>
+                                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjRqNL1htWEAxW5vokEHXSvD0UQFnoECCgQAQ&url=https%3A%2F%2Fwww.rocketleague.com%2Fen&usg=AOvVaw2A9DwjqwHBHPGL5ByVrM7v&opi=89978449">
+                                    <button>Visit Website</button>
+                                </a>
+                            </div>
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Games;
